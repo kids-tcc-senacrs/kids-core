0e989d5060a64c125f8771c348a8c45a4dabe1cc
“Luciano <lucianoortizsilva@gmail.com>
2017-06-18 23:38:17 -0300

    #5

--- src/main/java/com/kids/WebConfig.java
+++ src/main/java/com/kids/WebConfig.java
@@ -11,9 +11,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	
 	@Override
 	public void addCorsMappings(final CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedHeaders("Access-Control-Allow-Origin", "*")
-			.allowCredentials(true).maxAge(3600);
+		  registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
+          .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                  "Access-Control-Request-Headers")
+          .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+          .allowCredentials(true).maxAge(3600);
 	}
 	
 }
