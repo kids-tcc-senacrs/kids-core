c434bbb2f0725936443ebdb111c6354e8536b236
ortiz <luciano.ortiz@neogrid.com>
2017-07-27 11:47:34 -0300

    #8

--- src/main/java/com/kids/BootApplication.java
+++ src/main/java/com/kids/BootApplication.java
@@ -10,13 +10,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import com.google.common.base.Predicates;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import com.google.common.base.Predicates;
 
 /**
  * 
@@ -31,39 +31,32 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableJpaRepositories
 public class BootApplication {
 
-    public static void main(String[] args) {
-	SpringApplication.run(BootApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
 
+			@Override
+			public void addCorsMappings(final CorsRegistry registry) {
+				registry.addMapping("/**")//
+						.allowedOrigins("*")//
+						.allowedMethods("GET", "POST", "OPTIONS", "PUT")//
+						.allowedHeaders("Content-Type")//
+						.allowCredentials(true)//
+						.maxAge(3600);//
+			}
+		};
+	}
 
-
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-	return new WebMvcConfigurerAdapter() {
-
-	    @Override
-	    public void addCorsMappings(final CorsRegistry registry) {
-		registry.addMapping("/**")//
-		        .allowedOrigins("http://localhost:4200", "https://kids-web.herokuapp.com")//
-		        .allowedMethods("GET", "POST", "OPTIONS", "PUT")//
-		        .allowCredentials(true)//
-		        .maxAge(3600);//
-	    }
-	};
-    }
-
-
-
-
-
-    @Bean
-    public Docket api() {
-	return new Docket(DocumentationType.SWAGGER_2)//
-	        .select().apis(RequestHandlerSelectors.any())//
-	        .paths(Predicates.not(PathSelectors.regex("/error")))//
-	        .build();//
-    }
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)//
+				.select().apis(RequestHandlerSelectors.any())//
+				.paths(Predicates.not(PathSelectors.regex("/error")))//
+				.build();//
+	}
 
 }
