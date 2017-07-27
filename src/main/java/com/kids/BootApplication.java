8d3990d2e377a29d23afea0861f8537e97250177
lucianoortizsilva <lucianoortizsilva@gmail.com>
2017-07-26 21:08:02 -0300

    #8.

--- src/main/java/com/kids/BootApplication.java
+++ src/main/java/com/kids/BootApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.google.common.base.Predicates;
 
@@ -36,23 +39,24 @@ public class BootApplication {
 
 
 
-    //    @Bean
-    //    public WebMvcConfigurer corsConfigurer() {
-    //	return new WebMvcConfigurerAdapter() {
-    //
-    //	    @Override
-    //	    public void addCorsMappings(CorsRegistry registry) {
-    //		registry.addMapping("/**")//
-    //		        .allowedOrigins("http://localhost:4200", "https://kids-web.herokuapp.com")//
-    //		        .allowedMethods("GET", "POST", "OPTIONS", "PUT")//
-    //		        .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers")//
-    //		        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")//
-    //		        .allowCredentials(true)//
-    //		        .maxAge(3600);//
-    //	    }
-    //	};
-    //    }
-    //
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+	return new WebMvcConfigurerAdapter() {
+
+	    @Override
+	    public void addCorsMappings(final CorsRegistry registry) {
+		registry.addMapping("/**")//
+		        .allowedOrigins("http://localhost:4200", "https://kids-web.herokuapp.com")//
+		        .allowedMethods("GET", "POST", "OPTIONS", "PUT")//
+		        .allowCredentials(true)//
+		        .maxAge(3600);//
+	    }
+	};
+    }
+
+
+
+
 
     @Bean
     public Docket api() {
--- src/main/java/com/kids/WebSecurityConfig.java
+++ src/main/java/com/kids/WebSecurityConfig.java
@@ -1,6 +1,6 @@
 package com.kids;
 
-import static com.kids.util.KidsConstant.URL_WEB_DEV;
+import static com.kids.util.KidsConstant.URL_WEB_PROD;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,16 +10,10 @@ import org.springframework.security.web.header.writers.StaticHeadersWriter;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private static final String URL_AUTORIZADA = URL_WEB_DEV;
-
-
-
-
-
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
 	http.headers()//
-	        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", URL_AUTORIZADA));
+	        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", URL_WEB_PROD));
 
     }
 }
