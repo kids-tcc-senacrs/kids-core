058f741faaf438a0f81013945c93419a0afdbb51
ortiz <luciano.ortiz@neogrid.com>
2017-07-27 13:56:38 -0300

    #8

--- src/main/java/com/kids/moduloautenticacao/UsuarioRestController.java
+++ src/main/java/com/kids/moduloautenticacao/UsuarioRestController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,7 +46,7 @@ public class UsuarioRestController {
 
 
 
-
+    @CrossOrigin
     @RequestMapping(method = GET, path = "/{email:.+}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getUserByEmail(@PathVariable(required = true) final String email) {
 	final Usuario usuario = this.usuarioService.getUserByEmail(email);
