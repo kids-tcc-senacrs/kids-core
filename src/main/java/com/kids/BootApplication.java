e8e043c63c8f4d76cad596e0026443f243b14659
lucianoortizsilva <lucianoortizsilva@gmail.com>
2017-07-27 22:48:39 -0300

    #8 add auto configuration

--- src/main/java/com/kids/BootApplication.java
+++ src/main/java/com/kids/BootApplication.java
@@ -1,6 +1,7 @@
 package com.kids;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 
 @EntityScan(basePackageClasses = { BootApplication.class, Jsr310JpaConverters.class })
+@EnableAutoConfiguration
 @SpringBootApplication
 @EnableJpaRepositories
 public class BootApplication {
