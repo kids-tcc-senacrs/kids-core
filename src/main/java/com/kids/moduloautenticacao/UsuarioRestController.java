f16138877df1fb18eb9c282141cac9a9b8e2096d
lucianoortizsilva <lucianoortizsilva@gmail.com>
2017-08-09 18:02:48 -0300

    #8

--- src/main/java/com/kids/moduloautenticacao/UsuarioRestController.java
+++ src/main/java/com/kids/moduloautenticacao/UsuarioRestController.java
@@ -46,12 +46,12 @@ public class UsuarioRestController {
 
 
 
-    //@CrossOrigin(origins = { KidsConstant.URL_WEB_DEV, KidsConstant.URL_WEB_PROD })
+    @CrossOrigin(origins = { KidsConstant.URL_WEB_DEV, KidsConstant.URL_WEB_PROD })
     @RequestMapping(method = GET, path = "/{email:.+}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getUserByEmail(@PathVariable(required = true) final String email) {
 	final Usuario usuario = this.usuarioService.getUserByEmail(email);
 	final HttpStatus httpStatus = usuario == null ? HttpStatus.NO_CONTENT : HttpStatus.OK;
-	return ResponseEntity.status(httpStatus).header("Access-Control-Allow-Origin", "*").body(KidsJsonUtil.convertToJson(usuario));//
+	return ResponseEntity.status(httpStatus).body(KidsJsonUtil.convertToJson(usuario));//
     }
 
 
