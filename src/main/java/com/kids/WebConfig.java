68dbff4e2ccc70e3d8f0b916d702d22d809411fc
ortiz <luciano.ortiz@neogrid.com>
2017-08-28 11:00:10 -0300

    tentativa de corrigir o problema de cors

--- src/main/java/com/kids/WebConfig.java
+++ src/main/java/com/kids/WebConfig.java
@@ -1,20 +1,20 @@
 package com.kids;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+//@Configuration
+public class WebConfig {
+	// public class WebConfig extends WebMvcConfigurerAdapter {
 
-    @Override
-    public void addCorsMappings(final CorsRegistry registry) {
-	registry.addMapping("/**")//
-	        .allowedOrigins("*")//
-	        .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")//
-	        .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers")//
-	        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")//
-	        .allowCredentials(true)//
-	        .maxAge(3600);//
-    }
+	// @Override
+	// public void addCorsMappings(final CorsRegistry registry) {
+	// registry.addMapping("/**")//
+	// .allowedOrigins("*")//
+	// .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")//
+	// .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin",
+	// "Access-Control-Request-Method", "Access-Control-Request-Headers")//
+	// .exposedHeaders("Access-Control-Allow-Origin",
+	// "Access-Control-Allow-Credentials")//
+	// .allowCredentials(true)//
+	// .maxAge(3600);//
+	// }
 }
