ea1887576dd685fc3155a45754d44d28175a82f6
lucianoortizsilva <lucianoortizsilva@gmail.com>
2017-08-30 20:55:22 -0300

    cors

--- src/main/java/com/kids/WebConfig.java
+++ src/main/java/com/kids/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	registry.addMapping("/**")//
 	        .allowedOrigins("*")//
 	        .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")//
-	        .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers")//
+	        .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers")//	        
 	        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")//
 	        .allowCredentials(true)//
 	        .maxAge(3600);//
